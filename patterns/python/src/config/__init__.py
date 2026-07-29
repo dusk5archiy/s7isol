5629ba9319c433074b4a1405f12cffdc5232a1a7
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-29 11:46:23 +0700

    simplify

--- patterns/python/src/config/__init__.py
+++ patterns/python/src/config/__init__.py
@@ -5,6 +5,7 @@ import yaml
 
 def get_config[T: BaseModel](stem: str, config_cls: Type[T]) -> T:
     with open(f"configs/config/{stem}.yaml", encoding="utf-8") as f:
-        c = config_cls(**yaml.safe_load(f))
+        data = yaml.safe_load(f)
+        c = config_cls.model_validate(data)
 
     return c
