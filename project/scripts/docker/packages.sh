1d5ca74fe93c5b55fa287ab7731a95b2c836cee5
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-30 13:57:54 +0700

    debug with +x

--- project/scripts/docker/packages.sh
+++ project/scripts/docker/packages.sh
@@ -7,6 +7,7 @@ ubuntu)
   apt-get update
   apt-get install -y --no-install-recommends \
     sudo git
+  git config --global http.sslVerify false
   ;;
 *)
   echo "[-- unsupported platform --]" >&2
