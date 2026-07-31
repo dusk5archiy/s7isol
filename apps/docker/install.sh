8a3008d5497192450ae0992f61a39e945ef5c283
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-31 11:38:15 +0700

    update bazel and docker apts

--- apps/docker/install.sh
+++ apps/docker/install.sh
@@ -3,8 +3,7 @@
 case "$(source /etc/os-release && echo $ID)" in
 ubuntu)
   # Add Docker's official GPG key:
-  sudo apt update
-  sudo apt install ca-certificates curl
+  sudo apt-get install -y --no-install-recommends ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -19,8 +18,9 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-  sudo apt update
-  sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends \
+    docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo systemctl start docker
   sudo systemctl enable docker
   ;;
