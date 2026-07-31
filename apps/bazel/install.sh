0cbca1acbef66a7d780b19773e10253504249c6b
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-31 11:15:45 +0700

    add bazel and cuda-toolkit

---
+++ apps/bazel/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends bazel-7.7.0
+sudo cp /usr/bin/bazel-7.7.0 /usr/bin/bazel
---
+++ apps/cuda-toolkit/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd /tmp
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2604/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends cuda-toolkit-13-3
