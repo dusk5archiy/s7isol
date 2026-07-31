9387a94178427f67bb222b89e4f1952defd28c6d
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-31 16:58:13 +0700

    update cuda includes

--- apps/cuda-toolkit/install.sh
+++ apps/cuda-toolkit/install.sh
@@ -7,4 +7,5 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2604/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends cuda-toolkit-13-3 libcudnn9-dev-cuda-13
-sudo ln -snf /usr/include/cudnn*.h /usr/local/cuda/include/
+sudo ln -snf /usr/include/x86_64-linux-gnu/cudnn*.h /usr/local/cuda/include/
+sudo ln -snf /usr/lib/x86_64-linux-gnu/libcudnn* /usr/local/cuda/lib64/
