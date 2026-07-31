e703656d4b39b9fca45c3f8caab5f7457abc1827
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-31 17:07:10 +0700

    updated symlinks

--- apps/cuda-toolkit/install.sh
+++ apps/cuda-toolkit/install.sh
@@ -9,3 +9,4 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends cuda-toolkit-13-3 libcudnn9-dev-cuda-13
 sudo ln -snf /usr/include/x86_64-linux-gnu/cudnn*.h /usr/local/cuda/include/
 sudo ln -snf /usr/lib/x86_64-linux-gnu/libcudnn* /usr/local/cuda/lib64/
+sudo ln -snf /usr/local/cuda/lib64 /usr/local/cuda/lib
