f96e7b8a8f7dba356bfb8a3097f4df98feb2d246
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-07-31 12:02:01 +0700

    add wget

--- apps/bazel/install.sh
+++ apps/bazel/install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-sudo apt-get install -y --no-install-recommends apt-transport-https curl gnupg
+sudo apt-get install -y --no-install-recommends apt-transport-https curl gnupg wget
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
 sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
