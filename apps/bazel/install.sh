52fc09678582f6091f174b7920e62121cda431f9
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-08-07 10:29:59 +0700

    add chmod to bazel

--- apps/bazel/install.sh
+++ apps/bazel/install.sh
@@ -7,3 +7,4 @@ if command -v bazel &>/dev/null; then
 fi
 
 sudo curl -fsSL https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-amd64 -o /usr/local/bin/bazel
+sudo chmod +x /usr/local/bin/bazel
