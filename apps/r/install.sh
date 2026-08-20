30b05aa8b8d54bee62de0fd8519cd1d2916b6566
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-08-20 08:15:54 +0700

    add r installation and yazi config

--- apps/r/install.sh
+++ apps/r/install.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
-wget https://rstudio.org/download/latest/stable/desktop/jammy/rstudio-latest-amd64.deb -O /tmp/rstudio-latest-amd64.deb
-
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
-  r-base r-base-dev \
-  /tmp/rstudio-latest-amd64.deb
+  r-base r-base-dev
+
+sudo snap install rstudio --classic
+sudo apt-get install libcurl4-openssl-dev
