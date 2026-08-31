44125741064095ab58e896e741043191284d3627
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-08-31 18:03:00 +0700

    simplify docker for arch

--- docker/base/s7isol.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-bash "$HOME/s7isol/install.sh" --profile
--- projects/docker/env.sh
+++ projects/docker/env.sh
@@ -4,21 +4,21 @@ CONFIG_PROJECT_NAME=$BaseName
 CONFIG_DOCKERFILE=$PWD/docker/Dockerfile
 
 export CONFIG_PROJECT_NAME CONFIG_DOCKERFILE
-
-# ------------------------------------------------------------------------------
-
 export BUILDX_NO_DEFAULT_ATTESTATIONS=1
 export CONFIG_MOUNT_FALLBACK=/dev/null:/dev/null
 export CONFIG_CONTEXT=$PWD
 export CONFIG_USER_NAME=skjuser
-CONFIG_UID=$(id -u)
 
 # XDG Runtime ------------------------------------------------------------------
 
-HOST_XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/run/user/$CONFIG_UID}
+ConfigUid=$(id -u)
+HostXdgRuntimeDir=${XDG_RUNTIME_DIR:-/run/user/$ConfigUid}
 CLIENT_XDG_RUNTIME_DIR=/home/$CONFIG_USER_NAME/sockets
 export CONFIG_XDG_RUNTIME_DIR=$CLIENT_XDG_RUNTIME_DIR
 export CONFIG_ENV_XDG_RUNTIME_DIR=XDG_RUNTIME_DIR=$CLIENT_XDG_RUNTIME_DIR
+
+# Fallbacks --------------------------------------------------------------------
+export CONFIG_ENV_FALLBACK=$CONFIG_ENV_XDG_RUNTIME_DIR
 
 # Workspace --------------------------------------------------------------------
 
@@ -26,10 +26,6 @@ FROM_WORKSPACE=$CONFIG_CONTEXT
 TO_WORKSPACE=/home/$CONFIG_USER_NAME/workspace
 export CONFIG_WORKSPACE=$TO_WORKSPACE
 export CONFIG_MOUNT_WORKSPACE=$FROM_WORKSPACE:$TO_WORKSPACE
-
-# Fallbacks --------------------------------------------------------------------
-
-export CONFIG_ENV_FALLBACK=$CONFIG_ENV_XDG_RUNTIME_DIR
 
 # XDG Session ------------------------------------------------------------------
 
@@ -48,20 +44,20 @@ if [[ -s $FROM_X11 ]]; then export CONFIG_MOUNT_X11=$FROM_X11:$TO_X11:rw; fi
 if [[ -n ${WAYLAND_DISPLAY:-} ]]; then
   export CONFIG_ENV_WAYLAND_DISPLAY=WAYLAND_DISPLAY=$WAYLAND_DISPLAY
 
-  FROM_WAYLAND_SOCKET=$HOST_XDG_RUNTIME_DIR/$WAYLAND_DISPLAY
+  FROM_WAYLAND_SOCKET=$HostXdgRuntimeDir/$WAYLAND_DISPLAY
   TO_WAYLAND_SOCKET=$CLIENT_XDG_RUNTIME_DIR/$WAYLAND_DISPLAY
   if [[ -S $FROM_WAYLAND_SOCKET ]]; then export CONFIG_MOUNT_WAYLAND_SOCKET=$FROM_WAYLAND_SOCKET:$TO_WAYLAND_SOCKET; fi
 fi
 
 # Pipewire ---------------------------------------------------------------------
 
-FROM_PIPEWIRE_SOCKET=$HOST_XDG_RUNTIME_DIR/pipewire-0
+FROM_PIPEWIRE_SOCKET=$HostXdgRuntimeDir/pipewire-0
 TO_PIPEWIRE_SOCKET=$CLIENT_XDG_RUNTIME_DIR/pipewire-0
 if [[ -S $FROM_PIPEWIRE_SOCKET ]]; then export CONFIG_MOUNT_PIPEWIRE_SOCKET=$FROM_PIPEWIRE_SOCKET:$TO_PIPEWIRE_SOCKET; fi
 
 # Pulse ------------------------------------------------------------------------
 #
-FROM_PULSE_SOCKET=$HOST_XDG_RUNTIME_DIR/pulse
+FROM_PULSE_SOCKET=$HostXdgRuntimeDir/pulse
 TO_PULSE_SOCKET=$CLIENT_XDG_RUNTIME_DIR/pulse
 if [[ -d $FROM_PULSE_SOCKET ]]; then export CONFIG_MOUNT_PULSE_SOCKET=$FROM_PULSE_SOCKET:$TO_PULSE_SOCKET; fi
 
