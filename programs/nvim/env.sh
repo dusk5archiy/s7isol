ede148b9db42b0acb17a2aa8cbaa33a815716cfb
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-09-13 14:16:38 +0700

    update S7ISOL paths

--- programs/nvim/env.sh
+++ programs/nvim/env.sh
@@ -1,2 +1,8 @@
-export NVIM_CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/nvim
-export S7ISOL_NVIM_CONFIG_DIR=${S7ISOL_NVIM_CONFIG_DIR:-$S7ISOL/config/nvim}
+Main() {
+  local S7isol && S7isol=$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")
+  export NVIM_CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/nvim
+  export S7ISOL_NVIM_CONFIG_DIR=${S7ISOL_NVIM_CONFIG_DIR:-$S7isol/config/nvim}
+}
+
+Main "$@"
+unset -f Main
